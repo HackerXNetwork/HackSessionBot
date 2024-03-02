4ebbe332029ef4336112e72994814b87c47d4709
HACKER X NETWORK <159551041+HackerXNetwork@users.noreply.github.com>
2024-03-02 21:39:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,31 @@
-class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
-  "addons": [],
+{
+    "name": "LEGENDX BOT",
+    "description": "A bot CAN DO ANYTHING",
+    "logo": "",
+    "keywords": [
+        "TELETHON",
+        "X ARMY",
+        "Telegram Bot",
+        "piro bolte",
+        "LEGENDX22"
+    ],
+    "website": "",
+    "success_url": "https://telegram.dog/LEGENDX22",
+    "env": {
+        "BOT_TOKEN": {
+            "description": "You Telegram Bot Token from @BotFather",
+            "value": ""
+        },
+        "API_HASH": {
+            "description": "Your API Hash from my.telegram.org",
+            "value": ""
+        },
+        "APP_ID": {
+            "description": "Your API ID from my.telegram.org",
+            "value": ""
+        }
+    },
+    "addons": [],
     "buildpacks": [
         {
             "url": "heroku/python"
@@ -16,3 +37,4 @@ class Config:
             "size": "free"
         }
     }
+}
